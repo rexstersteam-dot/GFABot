81028a65cc7b805d862929ccaa16601a745cd957
rexstersteam-dot <rexster.steam@gmail.com>
2026-06-16 21:46:42 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Official GFA Bot.",
         // Activity type number (0 = Playing).
         type: 0,
       },
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#FEE75C",
+      secondary: "#000000",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
